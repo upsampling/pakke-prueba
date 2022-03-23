401ea12bb1f39ab431adc994688190deb61dca96
Erasmo_D <erasmo.dejesus@outlook.com>
2022-03-23 00:15:59 -0600

    Ã[FIX] clean function object with recursivity

--- src/features/Object/controllers/CleanObject.js
+++ src/features/Object/controllers/CleanObject.js
@@ -1,24 +1,22 @@
-const { validate } = require('jsonschema');
-
-const objectSchema = require('../validators/objectSchema.json');
-
 function CleanObject (req, res) {
   const bodyAttributes = req.body;
 
   if (Object.entries(bodyAttributes).length === 0) {
     return res.status(400).send('Void object');
   }
-
-  const { valid, errors } = validate(bodyAttributes, objectSchema);
-
-  if (!valid) {
-    return res.status(400).send({ message: errors.map(item => { return item.message; }) });
-  }
-
-  const objectClean = Object.fromEntries(Object.entries(bodyAttributes).filter(([_, v]) => v != null));
-  objectClean.hola = Object.fromEntries(Object.entries(objectClean.hola).filter(([_, v]) => v != null));
-
-  return res.status(201).send(objectClean);
+  const response = removeEmpty(bodyAttributes);
+  return res.status(201).send(response);
 }
 
+function removeEmpty (obj) {
+  return Object.entries(obj)
+    .filter(([_, v]) => {
+      if (v === null || v === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .reduce((acc, [k, v]) => ({ ...acc, [k]: v === Object(v) ? removeEmpty(v) : v }), {});
+}
 module.exports = { CleanObject };
